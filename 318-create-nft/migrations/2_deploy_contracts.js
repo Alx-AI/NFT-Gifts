ff970e56ce937ba16ec2afedabf2d9577df625d2
AlxDfy <ajduffay@gmail.com>
2021-10-12 14:54:39 -0700

    Fix

--- 318-create-nft/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const NFT = artifacts.require("NFT");
-
-module.exports = async function (deployer, _network, accounts) {
-  await deployer.deploy(NFT);
-  const nft = await NFT.deployed();
-  await nft.mint(accounts[0]);
-};
